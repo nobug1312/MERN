9897df0430220a4560eaf20746ee8d0c38cd7239
kvi <khai26111999@gmail.com>
2023-11-14 20:22:33 +0700

    setup

---
+++ mern/src/App.jsx
@@ -0,0 +1,12 @@
+
+import Button from '@mui/material/Button'
+function App() {
+  return (
+    <>
+      <div className="">heheh</div>
+      <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default App
